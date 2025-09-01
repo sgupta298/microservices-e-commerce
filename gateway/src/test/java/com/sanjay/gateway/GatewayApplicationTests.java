f2a4e0584de47da2a663ae00f6caf14c38fef235
Sanjay Gupta <sanjaykrgupta298@gmail.com>
2025-09-02 02:28:11 +0530

    commiting unversioned files

---
+++ gateway/src/test/java/com/sanjay/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
